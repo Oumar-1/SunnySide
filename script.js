3e011519b379cb86bb259947a0b9e814d8f4a860
oumar <oumar2000mouhamed@gmail.com>
2022-05-06 18:54:53 +0300

    a final project from the sunnySide challange on Fron End mentor

---
+++ script.js
@@ -0,0 +1,5 @@
+let menu = document.querySelector(".menu")
+let menuToggle = document.querySelector(".hamburger-icon");
+console.log(menu)
+
+menuToggle.onclick= () => menu.classList.toggle("active");
